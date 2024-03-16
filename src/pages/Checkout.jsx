d3b77cc1cc97e8b413f764fba370b972b5c4d254
sluggerbeast <saurabhpareek0000@gmail.com>
2024-03-16 06:12:47 +0000

    dev commits

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,21 +1,19 @@
 import React, { useEffect, useState } from "react";
 import CartItems from "../components/CartItems";
-
+import { useNavigate } from "react-router-dom";
 ///{id:1,productName:"Watch", price:"10",category:""}
-export default function Checkout({ cartList, onCartEdit,CartRef }) {
+export default function Checkout({ cartList, onCartEdit, CartRef }) {
   let totalCost = 0;
+  const navigate = useNavigate();
   const [payPressed, setPayPressed] = useState(false);
   //console.log(cartList);
-  useEffect(()=>{
+  useEffect(() => {
     CartRef.current.style.display = "none";
-  },[])
+  }, []);
 
-  
-  function handlePayClick(){
+  function handlePayClick() {
     setPayPressed(true);
-
   }
-
 
   return (
     <>
@@ -37,48 +35,66 @@ export default function Checkout({ cartList, onCartEdit,CartRef }) {
             );
           })}
         </div>
-        {cartList.length>0 && <div className="bg-slate-50 w-[70vw] lg:w-[50vw] pt-3 ">
-          <div className="pb-3">Total: <i className="fa fa-inr mr-1" />{totalCost}</div>
-          <button onClick={handlePayClick} className="bg-black text-white p-3 w-[70vw] lg:w-[50vw]">
-            { !payPressed? <>Pay <i className="fa fa-inr mr-1 ml-1" />{totalCost}</>:
-             <><i class="fa fa-spinner animate-spin text-blue-100 mr-1" />
-           Processing...</>}
-          </button>
-         
-        </div>
-        }
+        {cartList.length > 0 ? (
+          <div className="bg-slate-50 w-[70vw] lg:w-[50vw] pt-3 ">
+            <div className="pb-3">
+              Total: <i className="fa fa-inr mr-1" />
+              {totalCost}
+            </div>
+            <button
+              onClick={handlePayClick}
+              className="bg-black text-white p-3 w-[70vw] lg:w-[50vw]"
+            >
+              {!payPressed ? (
+                <>
+                  Pay <i className="fa fa-inr mr-1 ml-1" />
+                  {totalCost}
+                </>
+              ) : (
+                <>
+                  <i class="fa fa-spinner animate-spin text-blue-100 mr-1" />
+                  Processing...
+                </>
+              )}
+            </button>
+          </div>
+        ) : (
+          // if cart is empty during checkout redirect to home page.
+          navigate("/") 
+        )}
       </div>
-      
-          
-      {payPressed && <div>
-            <CheckoutBtn />
-             </div>}
+
+      {payPressed && (
+        <div>
+          <CheckoutBtn />
+        </div>
+      )}
     </>
   );
 }
 
-import { loadStripe } from '@stripe/stripe-js';
+import { loadStripe } from "@stripe/stripe-js";
 
-function prePay(){
+function prePay() {
   let stripePromise;
-  const NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY= "pk_test_51OspyvSDjv5U4D3yA3W31pcuPIZALD0EWATfEvSp9pZ2vXB8RZT7iek8wLtmmZ3BDuym7CyjVl7i0HK3i4J6N9QA00gG8U9ELn"
+  const NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY =
+    "pk_test_51OspyvSDjv5U4D3yA3W31pcuPIZALD0EWATfEvSp9pZ2vXB8RZT7iek8wLtmmZ3BDuym7CyjVl7i0HK3i4J6N9QA00gG8U9ELn";
   if (!stripePromise) {
     stripePromise = loadStripe(NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
-  
-  return stripePromise;
-};
 
+    return stripePromise;
+  }
 }
 
 function CheckoutBtn() {
-  const SK = "sk_test_51OspyvSDjv5U4D3yQRFj07bOJ87TBKniGSMMRw5zUM8Fz8fAmsBIdEYkjghXEo8FrWseTIsZXp9Zn90yurvoYgA800tXIIhiFM"
-  const NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY= "pk_test_51OspyvSDjv5U4D3yA3W31pcuPIZALD0EWATfEvSp9pZ2vXB8RZT7iek8wLtmmZ3BDuym7CyjVl7i0HK3i4J6N9QA00gG8U9ELn"
-  const NEXT_PUBLIC_STRIPE_PRICE_ID='price_my_id'
+  const SK =
+    "sk_test_51OspyvSDjv5U4D3yQRFj07bOJ87TBKniGSMMRw5zUM8Fz8fAmsBIdEYkjghXEo8FrWseTIsZXp9Zn90yurvoYgA800tXIIhiFM";
+  const NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY =
+    "pk_test_51OspyvSDjv5U4D3yA3W31pcuPIZALD0EWATfEvSp9pZ2vXB8RZT7iek8wLtmmZ3BDuym7CyjVl7i0HK3i4J6N9QA00gG8U9ELn";
+  const NEXT_PUBLIC_STRIPE_PRICE_ID = "price_my_id";
   const [isProcessing, setIsProcessing] = useState(false);
 
-  
-    
-  useEffect(()=>{
+  useEffect(() => {
     async function handleCheckout() {
       const stripe = await prePay();
       const { error } = await stripe.redirectToCheckout({
@@ -88,26 +104,20 @@ function CheckoutBtn() {
             quantity: 1,
           },
         ],
-        mode: 'subscription',
-        successUrl: `https://pareekbackend.netlify.app/api/success`,
+        mode: "subscription",
+        successUrl: `https://pareekstore.netlify.app/`,
         cancelUrl: `https://pareekstore.netlify.app/`,
-        customerEmail: 'customer@email.com',
+        customerEmail: "customer@email.com",
       });
       console.log(error.message);
     }
 
     handleCheckout();
-  })
- 
+  });
 
   return (
     <>
-    <h1>Processing payment...</h1>
+      <h1>Processing payment...</h1>
     </>
-  )
+  );
 }
-
-
-
-
-
